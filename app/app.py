e361ba3ce91ca37d53e6f97f64e13806e8e1897c
gio <giomikecs17@gmail.com>
2024-03-01 08:00:03 -0500

    interface streamlit v1.0

---
+++ app/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+
+st.markdown('# Welcome to my app')
+
+line = st.slider('Selecciona valor', 0, 10, 3)
+
+def get_data():
+    return pd.DataFrame(
+            np.random.randn(3, 3),
+            columns=['a', 'b', 'c'])
+
+@st.cache_data
+def get_cached_data():
+    return get_data()
+
+columns = st.columns(2)
+
+columns[0].write("Uncached dataframe")
+columns[0].write(get_data())
+
+columns[1].write("Cached dataframe")
+columns[1].write(get_cached_data())
---
+++ app/pages/page_01.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.markdown('# This is page 1')
